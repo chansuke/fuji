24bbb0a247428c48a1d9bac1091535bc3b3660b4
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2015-10-25 21:08:30 +0900

    switched toml for app.go.

--- app.go
+++ app.go
@@ -20,12 +20,12 @@ import (
 	"github.com/shiguredo/fuji/broker"
 	"github.com/shiguredo/fuji/device"
 	"github.com/shiguredo/fuji/gateway"
-	"github.com/shiguredo/fuji/inidef"
+	"github.com/shiguredo/fuji/toml"
 )
 
 // Start make command channel and start gateway.
 func Start(configPath string) {
-	conf, err := inidef.LoadConfig(configPath)
+	conf, err := toml.LoadConfig(configPath)
 	if err != nil {
 		log.Fatalf("loading ini file faild, %v", err)
 	}
@@ -39,7 +39,7 @@ func Start(configPath string) {
 }
 
 // StartByFileWithChannel starts Gateway with command Channel
-func StartByFileWithChannel(conf inidef.Config, commandChannel chan string) error {
+func StartByFileWithChannel(conf toml.Config, commandChannel chan string) error {
 	gw, err := gateway.NewGateway(conf)
 	if err != nil {
 		log.Fatalf("gateway create error, %v", err)
